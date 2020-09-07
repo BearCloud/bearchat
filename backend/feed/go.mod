5122259645e7de45a9264abb8af6d287024eb98f
ryansearcy <8144773+ryansearcy@users.noreply.github.com>
2020-09-07 00:23:40 -0700

    updated api calls

--- backend/feed/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/BearCloud/fa20-project-dev/backend/feed
-
-go 1.15
--- backend/friends/api/api.go
+++ backend/friends/api/api.go
@@ -11,9 +11,11 @@ import (
 )
 
 func RegisterRoutes(router *mux.Router) error {
-	router.HandleFunc("/api/friends/{uuid}", areFriends).Methods(http.MethodPost)
-	router.HandleFunc("/api/friends/{uuid}/mutual", mutualFriends).Methods(http.MethodPost)
-  router.HandleFunc("/api/friends/get", getFriends).Methods(http.MethodPost)
+	router.HandleFunc("/api/friends/{uuid}", areFriends).Methods(http.MethodGet)
+  router.HandleFunc("/api/friends/{uuid}", addFriend).Methods(http.MethodPost)
+  router.HandleFunc("/api/friends/{uuid}", deleteFriend).Methods(http.MethodDelete)
+	router.HandleFunc("/api/friends/{uuid}/mutual", mutualFriends).Methods(http.MethodGet)
+  router.HandleFunc("/api/friends", getFriends).Methods(http.MethodGet)
 
 	return nil
 }
