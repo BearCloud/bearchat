8f4e93d13e96d533b4fb29f0cbf07f0d5d360212
Ariful Rigan <ari.rigan@gmail.com>
2020-09-07 18:03:40 -0700

    feat: update database schema to support usernames and use uuid as primary key

--- backend/auth-database/initdb.sql
+++ backend/auth-database/initdb.sql
@@ -4,10 +4,11 @@ CREATE DATABASE auth;
 USE auth;
 
 CREATE TABLE users (
+    username VARCHAR(20),
     email VARCHAR(320),
     hashedPassword TEXT,
     verified boolean,
     resetToken TEXT,
     verifiedToken TEXT,
-    userId VARCHAR(128)
+    userId VARCHAR(128) PRIMARY KEY
 );
