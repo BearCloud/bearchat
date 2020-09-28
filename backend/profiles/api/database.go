6e13ecedfeef2e483d0b197b6c2e1755fc6e5d6b
ryansearcy <8144773+ryansearcy@users.noreply.github.com>
2020-09-28 15:43:40 -0700

    print to log

--- backend/profiles/api/database.go
+++ backend/profiles/api/database.go
@@ -11,6 +11,7 @@ import (
 var DB *sql.DB
 
 func InitDB() *sql.DB {
+	log.Println("attempting connections")
 	var err error
 	DB, err = sql.Open("mysql", "root:root@tcp(172.28.1.2:3306)/profiles")
 
