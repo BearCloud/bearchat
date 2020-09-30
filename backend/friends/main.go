da4c475f6b20f8f95d99ebd7a18af5b37ddcb9f8
Ari Rigan <ari.rigan@gmail.com>
2020-09-30 15:20:35 -0700

    update cors settings

--- backend/friends/main.go
+++ backend/friends/main.go
@@ -27,7 +27,9 @@ func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 		// Set headers
+		w.Header().Set("Access-Control-Allow-Headers:", "Content-Type")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
