6f5715b9280db696e860f5801598cd35fbd1ff4c
Jie Luo <jieluo@berkeley.edu>
2020-11-14 14:30:46 -0800

    Add friends dockerfile

--- backend/friends/api/database.go
+++ backend/friends/api/database.go
@@ -8,7 +8,10 @@ var DB *gremlin.Client
 func InitDB() {
   auth := gremlin.OptAuthUserPass("root", "root")
   var err error
-	DB, err = gremlin.NewClient("IP:80/gremlin", auth)
+  if err := gremlin.NewCluster("https://neptune-endpoint:8182/gremlin"); err != nil {
+		panic(err)
+	}
+	DB, err = gremlin.NewClient("neptune-endpoint:8182/gremlin", auth)
 	_, err = DB.ExecQuery(`g.V()`)
 	if err != nil {
 		panic(err)
