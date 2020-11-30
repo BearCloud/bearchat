30b2522d5fc7c42440eb90ed1cb40c6218c5735c
Ari Rigan <ari.rigan@gmail.com>
2020-11-29 22:37:09 -0800

    added friends functions

--- backend/friends/api/api.go
+++ backend/friends/api/api.go
@@ -4,15 +4,18 @@ import (
 	"net/http"
 	"github.com/gorilla/mux"
 	"log"
+	"bytes"
 	"encoding/json"
 	"fmt"
 )
 
+const NeptuneURL = "http://database-1.cluster-cfqqv4t1qo18.us-east-1.neptune.amazonaws.com:8182/gremlin"
+
 func RegisterRoutes(router *mux.Router) error {
 	router.HandleFunc("/api/friends/{uuid}", areFriends).Methods(http.MethodGet)
 	router.HandleFunc("/api/friends/{uuid}", addFriend).Methods(http.MethodPost)
-	router.HandleFunc("/api/friends/{uuid}", deleteFriend).Methods(http.MethodDelete)
-	router.HandleFunc("/api/friends/{uuid}/mutual", mutualFriends).Methods(http.MethodGet)
+	// router.HandleFunc("/api/friends/{uuid}", deleteFriend).Methods(http.MethodDelete)
+	// router.HandleFunc("/api/friends/{uuid}/mutual", mutualFriends).Methods(http.MethodGet)
 	router.HandleFunc("/api/friends", getFriends).Methods(http.MethodGet)
 	router.HandleFunc("/api/friends", addUser).Methods(http.MethodPost)
 
@@ -33,77 +36,117 @@ func getUUID (w http.ResponseWriter, r *http.Request) (uuid string) {
 	}
 	log.Println(claims)
 
+	
+
 	return claims["UserID"].(string)
 }
 
 func getFriends (w http.ResponseWriter, r *http.Request) {
 	uuid := getUUID(w, r)
-	res, err := gremlinClient.Execute("g.V().has('uuid', '" + uuid + "').out('friends with').values('uuid')")
+	gp := "g.V().has('uuid', '" + uuid + "').out('friends with').values('uuid')"
+	response, err := makeNeptuneRequest(gq)
+	// var req_body map[string]string
+	// req_body["gremlin"] = "g.V().has('uuid', '" + uuid + "').out('friends with').values('uuid')"
+
+	// jsonValue, _ := json.Marshal(req_body)
+
+	// resp, err := http.Post(NeptuneURL, "application/json", bytes.NewBuffer(jsonValue))
+	// defer resp.Body.Close()
+
+	// var response map[string]interface{}
+	
+	// err = json.NewDecoder(resp.Body).Decode(&response)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		log.Print(err.Error())
-	}
-
-	resJSON, err := json.Marshal(res[0].Result.Data)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		log.Print(err.Error())
-	}
-
-	fmt.Fprint(w, resJSON)
-}
-
-func addUser (w http.ResponseWriter, r *http.Request) {
-	uuid := getUUID(w, r)
-	_, err := gremlinClient.Execute("g.addV().property('uuid', '" + uuid + "')")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		log.Print(err.Error())
-	}
-}
-
-func areFriends(w http.ResponseWriter, r *http.Request) {
-	otherUUID := mux.Vars(r)["uuid"]
-	uuid := getUUID(w, r)
-  isFriend, err := gremlinClient.Execute("g.V().has('uuid', '" + uuid + "').out('friends with').where(otherV().has('uuid', '" + otherUUID + "')).count()")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		log.Print(err.Error())
 		return
 	}
-	json.NewEncoder(w).Encode(string(isFriend[0].Result.Data))
+
+	result := response["result"].(map[string]interface{})
+	data := result["data"].(map[string]interface{})
+	values := data["@value"].([]map[string]interface{})
+	
+	var friends []string
+	for _, value := range values {
+		uuid = value["@Value"].(string)
+		friends = append(friends, uuid)
+	}
+
+	json.NewEncoder(w).Encode(friends)
+	return
 }
 
 func addFriend(w http.ResponseWriter, r *http.Request) {
 	otherUUID := mux.Vars(r)["uuid"]
 	uuid := getUUID(w, r)
-	_, err := gremlinClient.Execute("g.addE('friends with').from(g.V().has('uuid', '" + uuid + "')).to(g.V().has('uuid', '" + otherUUID + "'))")
+	gq := "g.addE('friends with').from(g.V().has('uuid', '" + uuid + "')).to(g.V().has('uuid', '" + otherUUID + "'))"
+	response, err := makeNeptuneRequest(gq)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		log.Print(err.Error())
 		return
 	}
+	return
 }
 
-func deleteFriend(w http.ResponseWriter, r *http.Request) {
-	otherUUID := mux.Vars(r)["uuid"]
+func addUser (w http.ResponseWriter, r *http.Request) {
 	uuid := getUUID(w, r)
-  _, err := gremlinClient.Execute("g.V().bothE().filter(hasLabel('friends with')).where(inV().has('uuid', '" + uuid + "')).where(otherV().has('uuid', '" + otherUUID + "')).drop()")
+	gq := "g.addV().property('uuid', '" + uuid + "')"
+	response, err := makeNeptuneRequest(gq)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		log.Print(err.Error())
 		return
 	}
+	
+	return
 }
 
-func mutualFriends(w http.ResponseWriter, r *http.Request) {
+func areFriends(w http.ResponseWriter, r *http.Request) {
 	otherUUID := mux.Vars(r)["uuid"]
 	uuid := getUUID(w, r)
-	isFriend, err := gremlinClient.Execute("g.V().has('uuid', '" + uuid + "').both('friends with').and(both('friends with').has('uuid', '" +  otherUUID + "'))")
+	gq := "g.V().has('uuid', '" + uuid + "').out('friends with').where(otherV().has('uuid', '" + otherUUID + "')).count()"
+	response, err := makeNeptuneRequest(gq)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		log.Print(err.Error())
 		return
 	}
-	json.NewEncoder(w).Encode(isFriend[0].Result.Data)
+
+}
+
+// func deleteFriend(w http.ResponseWriter, r *http.Request) {
+// 	otherUUID := mux.Vars(r)["uuid"]
+// 	uuid := getUUID(w, r)
+//   _, err := gremlinClient.Execute("g.V().bothE().filter(hasLabel('friends with')).where(inV().has('uuid', '" + uuid + "')).where(otherV().has('uuid', '" + otherUUID + "')).drop()")
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 		log.Print(err.Error())
+// 		return
+// 	}
+// }
+
+// func mutualFriends(w http.ResponseWriter, r *http.Request) {
+// 	otherUUID := mux.Vars(r)["uuid"]
+// 	uuid := getUUID(w, r)
+// 	isFriend, err := gremlinClient.Execute("g.V().has('uuid', '" + uuid + "').both('friends with').and(both('friends with').has('uuid', '" +  otherUUID + "'))")
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 		log.Print(err.Error())
+// 		return
+// 	}
+// 	json.NewEncoder(w).Encode(isFriend[0].Result.Data)
+// }
+
+func makeNeptuneRequest(gremlinQuery string) (map[string]interface{}, error) {
+	var req_body map[string]string
+	req_body["gremlin"] = gremlinQuery
+	jsonValue, _ := json.Marshal(req_body)
+	resp, err := http.Post(NeptuneURL, "application/json", bytes.NewBuffer(jsonValue))
+	defer resp.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+	var response map[string]interface{}
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		return nil, err
+	}
+	return response, nil
 }
